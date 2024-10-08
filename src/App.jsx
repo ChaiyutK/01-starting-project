c66f7611282f86a834e61e45be1ed97a5bee3809
Chaiyut <benzchaiyut.k@gmail.com>
2024-10-08 23:52:55 +0700

    add ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import UserInput from "./components/UserInput";
 function App() {
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+    <UserInput />
+      <table id="result" className="center">
+        <thead>
+          <tr>
+            <th>Year</th>
+            <th>Insvestment Value</th>
+            <th>Interest (Year)</th>
+            <th>Total Interest</th>
+            <th>Invested Capital</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 export default App
